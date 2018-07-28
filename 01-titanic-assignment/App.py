47e646e2924028bc3484ee04dc1e3ecebdf70c39
Andrew Gahov <andrew.gahov@gmail.com>
2018-07-28 13:01:15 +0300

    Solution 4, 5, 6

--- 01-titanic-assignment/App.py
+++ 01-titanic-assignment/App.py
@@ -1,5 +1,6 @@
 import pandas
 import os
+import re
 
 # cd 01-titanic-assignment -- python3 App.py
 
@@ -40,7 +41,52 @@ def question2():
 
 
 def question3():
-    pass
+    # What percentage of the first class passengers were among all passengers?
+    classes_counts = data['Pclass'].value_counts()
+    answer3 = '{:.2f}'.format(classes_counts[1] * 100 / classes_counts.sum())
+    print_answer(3, answer3)
+
+
+def question4():
+    # What age were passengers? Calculate the average and the median.
+    describe = data['Age'].describe()
+    print_answer(4, '{:.2f} {:.2f}'.format(describe[1], describe[5]))
+
+
+def question5():
+    # Коррелируют ли число братьев/сестер с числом родителей/детей?
+    # Посчитайте корреляцию Пирсона между признаками SibSp и Parch.
+
+    # Коррелируют ли число братьев/сестер/супругов с числом родителей/детей?
+    # Посчитайте корреляцию Пирсона между признаками SibSp и Parch.
+
+    print_answer(5, '{:.2f}'.format(data['SibSp'].corr(data['Parch'])))
+
+
+def question6():
+    def clean_name(name):
+        # Первое слово до запятой - фамилия
+        s = re.search('^[^,]+, (.*)', name)
+        if s:
+            name = s.group(1)
+
+        # Если есть скобки - то имя пассажира в них
+        s = re.search('\(([^)]+)\)', name)
+        if s:
+            name = s.group(1)
+
+        # Удаляем обращения
+        name = re.sub('(Miss\. |Mrs\. |Ms\. )', '', name)
+
+        # Берем первое оставшееся слово и удаляем кавычки
+        name = name.split(' ')[0].replace('"', '')
+
+        return name
+
+    names = data[data['Sex'] == 'female']['Name'].map(clean_name)
+    name_counts = names.value_counts()
+    print_answer(6, name_counts.head(1).index.values[0])
+
 
 #
 #
@@ -53,4 +99,7 @@ print(data.columns)
 
 # question1()
 # question2()
-question3()
+# question3()
+# question4()
+# question5()
+question6()
